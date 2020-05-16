5bc3f2267fa81b077606d8352eead8f014f2fd6e
safroniaziz <safroni.aziz@gmail.com>
2020-05-16 10:05:30 +0700

    new commit

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -124,7 +124,7 @@
 
         <div class="section-title">
           <h2>GRAFIK NILAI TREN NON LINIER TERTINGGI SETIAP TAHUN</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+          <p>Ditempilkan menggunakan amcharts</p>
         </div>
 
         <div class="row">
